5966914350e129b2d03a09000a4e73a628b206b8
JoshT <wunjosh18@gmail.com>
2022-07-28 22:58:08 +0800

    Added ft_arr_len function

--- sources/input/input_utils.c
+++ sources/input/input_utils.c
@@ -1,5 +1,15 @@
 #include "../../includes/push_swap.h"
 
+size_t	ft_arr_len(char **arr)
+{
+	size_t	ret;
+
+	ret = 0;
+	while (arr[ret] != NULL)
+		ret++;
+	return (ret);
+}
+
 void	ft_put_error(void)
 {
 	ft_putstr_fd("Error\n", 2);
