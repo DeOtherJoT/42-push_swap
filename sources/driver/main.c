16324098e014527a5f8ef96813289c1105128e42
JoshT <wunjosh18@gmail.com>
2022-07-29 01:23:39 +0800

    updated main

--- sources/driver/main.c
+++ sources/driver/main.c
@@ -1,9 +1,16 @@
 #include "../../includes/push_swap.h"
 
+void	exit_prog(t_stacks *stacks)
+{
+	ft_stacks_del(stacks);
+	exit(EXIT_SUCCESS);
+}
+
 int	main(int argc, char *argv)
 {
 	t_stacks	*stacks;
 
 	process_input(stacks, argc, argv);
-	//sort_stacks(stacks);
+	sort_stacks(stacks);
+	exit_prog(stacks);
 }
